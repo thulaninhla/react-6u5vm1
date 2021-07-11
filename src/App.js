a1da1bf80962b5347aacc38a0902bb7d2463540d
thulaninhla <thulaninhlapo2@gmail.com>
2021-07-11 11:06:25 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,10 +22,15 @@ export default function CenteredGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>Header with Photo and name</Paper>
+          <Paper className={classes.paper}>
+            <Typography variant="h4">Thulani Nhlapo</Typography>
+            <Typography variant="h5">IT Graduate</Typography>
+          </Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}> contact, skills, language, hobbies</Paper>
+          <Paper className={classes.paper}>
+            
+          </Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>About Me, Education, work experience</Paper>
